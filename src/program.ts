a28b1c22755a241915f236dea6db3cbf6f32e519
Max Gilmour <mgilmour@maxgmr.ca>
2025-01-03 19:41:08 -0800

    add build tools

--- src/program.ts
+++ src/program.ts
@@ -6,10 +6,12 @@ const DESC = "Read Wikipedia articles in the terminal.";
 export const programInit = (argv: string[]): Command => {
   const program = new Command();
   program
+    .showHelpAfterError()
     .version(VERSION)
     .description(DESC)
     .option("-i, --interactive", "Prompt the user to select an article")
     .option("-s --strict", "Only open exact article title matches")
+    .argument("[Wikipedia search term]", "The article to search for", "")
     .parse(argv);
   return program;
 };
