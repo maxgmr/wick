6fc2cfe695d420aa587f2701cd310192ff52dfc5
Max Gilmour <mgilmour@maxgmr.ca>
2025-01-03 20:16:34 -0800

    impl CLI

--- src/program.ts
+++ src/program.ts
@@ -1,8 +1,14 @@
+import readline from "node:readline/promises";
+
 import { Command } from "commander";
+
 import { VERSION } from "./consts";
 
 const DESC = "Read Wikipedia articles in the terminal.";
 
+/**
+ * Initialises the program using the given args.
+ */
 export const programInit = (argv: string[]): Command => {
   const program = new Command();
   program
@@ -14,4 +20,28 @@ export const programInit = (argv: string[]): Command => {
     .argument("[Wikipedia search term]", "The article to search for", "")
     .parse(argv);
   return program;
+};
+
+/**
+ * If a Wikipedia search term was given in the CLI call, then return it. Otherwise,
+ * read a search term from stdin and return that.
+ */
+export const getProgramQuery = (program: Command): Promise<string> => {
+  if (program?.args?.[0]) {
+    // Search term was already given. Return it.
+    return new Promise((resolve) => {
+      resolve(program.args[0]);
+    });
+  }
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise((resolve) =>
+    rl.on("line", (res) => {
+      rl.close();
+      resolve(res);
+    }),
+  );
 };
--- src/wick.ts
+++ src/wick.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-import { programInit } from "./program";
+import { getProgramQuery, programInit } from "./program";
 
 (async () => {
   const program = programInit(process.argv);
   const options = program.opts();
   console.log(JSON.stringify(options, null, 2));
+  const query = await getProgramQuery(program);
+  console.log(`your query: <${query}>`);
 })();
